e28c50d1e9a3a916da41e6c127b08f5aa2ec0a48
A706\dam1 <raildetrenes@gmail.com>
2025-10-17 19:20:38 +0200

    Generaciones

--- src/main/java/org/example/ExcepcionHasNextInt.java
+++ src/main/java/org/example/ExcepcionHasNextInt.java
@@ -6,56 +6,60 @@ import java.util.Scanner;
 public class ExcepcionHasNextInt {
     static void main() {
         Scanner entrada = new Scanner(System.in);
+        final int ANIOMIN = 1900;
+        System.out.println("ELIGE MODO ");
+        System.out.println("[1] AÑO DE NACIMIENTO");
+        System.out.println("[2] EDAD");
+        if (entrada.hasNextInt()) {
+            int modo = entrada.nextInt();
+            LocalDateTime hoy = LocalDateTime.now();
 
-        System.out.println("ELIGE MODO 1 O MODO 2");
-        String modo = entrada.nextLine();
-        LocalDateTime hoy = LocalDateTime.now();
-        String eda;
-        int anio = 0;
-        int edad = 0;
-        boolean estado = false;
-        if (modo.equals("1")) {
-            try {
-                System.out.println("Introduce tu año de nacimiento");
-                eda = entrada.nextLine();
-                anio = Integer.parseInt(eda);
-                estado = true;
-            } catch (Exception e) {
-                System.out.println("Formato incorrecto");
-            }
-        } else if (modo.equals("2")) {
-            System.out.println("Introduce tu edad");
-            if (entrada.hasNextInt()){
-
-            edad = entrada.nextInt();
-            if (edad >= 0) {
-                anio = hoy.getYear() - edad;
-                estado = true;
+            int anio = 0;
+            boolean estado = false;
+            if (modo == 1) {
+                if (entrada.hasNextInt()){
+                    System.out.println("Introduce tu año de nacimiento");
+                    String eda = entrada.nextLine();
+                    anio = Integer.parseInt(eda);
+                    estado = true;
+                }else {
+                    System.out.println("Eso no es un año");
+                }
+            } else if (modo == 2) {
+                System.out.println("Introduce tu edad");
+                if (entrada.hasNextInt()) {
+                    int edad = entrada.nextInt();
+                    if (edad >= 0) {
+                        anio = hoy.getYear() - edad;
+                        estado = true;
+                    } else {
+                        System.out.println("No puedes tener una edad negativa");
+                    }
+                } else {
+                    System.out.println("Formato incorrecto");
+                }
             } else {
-                System.out.println("No puedes tener una edad negativa");
+                System.out.println("No has introducido un modo correcto");
             }
-        } else {
-            System.out.println("Formato incrrecto");
-        }
-    }else{
-            System.out.println("No has introducido un modo correcto");
-        }
-        if (estado){
-            if (anio >= 1900 && anio <=1927){
-                System.out.println("Sin generacion bautizada");
-            } else if (anio >= 1928 && anio <= 1944) {
-                System.out.println("Generacion silent");
-            } else if (anio >=1945 && anio <= 1964) {
-                System.out.println("Baby boomers");
-            } else if (anio >= 1965 && anio <=1981) {
-                System.out.println("Generacion X");
-            } else if (anio >= 1982 && anio<= 1994) {
-                System.out.println("Millenials");
-            } else if (anio >= 1995 && anio <= hoy.getYear()) {
-                System.out.println("Centenials");
-            }else{
-                System.out.println("No puede ser tu año de nacimiento");
+            if (estado) {
+                if (anio >= ANIOMIN && anio <= 1927) {
+                    System.out.println("Eres de la epoca sin generacion bautizada");
+                } else if (anio >= 1928 && anio <= 1944) {
+                    System.out.println("Eres de la generacion silent");
+                } else if (anio >= 1945 && anio <= 1964) {
+                    System.out.println("Eres de la generacion Baby boomers");
+                } else if (anio >= 1965 && anio <= 1981) {
+                    System.out.println("Eres de la generacion X");
+                } else if (anio >= 1982 && anio <= 1994) {
+                    System.out.println("Eres de la generacion millenials");
+                } else if (anio >= 1995 && anio <= hoy.getYear()) {
+                    System.out.println("Eres de la generacion centenials");
+                } else {
+                    System.out.println("No puede ser tu año de nacimiento");
+                }
             }
+        }else {
+            System.out.println("Valor del modo incorrecto");
         }
     }
 }
