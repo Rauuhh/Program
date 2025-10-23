08ee788ea9251fec13fa8c542520987bc553c0f6
A706\dam1 <raildetrenes@gmail.com>
2025-10-23 18:10:00 +0200

    Bateria Bucles

---
+++ src/main/java/org/example/BateriaFor.java
@@ -0,0 +1,78 @@
+package org.example;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class BateriaFor {
+    static void main() {
+        Scanner entrada = new Scanner(System.in);
+/*
+        System.out.println("Introduce tu contraseña");
+        String contra1 = entrada.nextLine();
+        System.out.println("Valida tu contraseña");
+        String contra2 = entrada.nextLine();
+        while (!contra2.equals(contra1)){
+            System.out.println("Inorrecto, vuelve a validar");
+            contra2 = entrada.nextLine();
+        }
+        System.out.println("Contraseña correcta");
+ */
+        /*
+        System.out.println("Introduce el numero por el que empezar la cuenta atras");
+        int numero = entrada.nextInt();
+        for (int i = numero; i >= 1; i--){
+            System.out.println(i);
+        }
+
+         */
+/*
+        for (int i = 1; i <= 50; i++){
+            if (i % 3 == 0){
+                System.out.println(i);
+            }
+        }
+ */
+        /*
+        System.out.println("Escribe una cadena");
+        String cadena = entrada.nextLine();
+        for (int i = 0; i < cadena.length(); i++) {
+            System.out.println(cadena.charAt(i));
+        }
+         */
+        /*
+        System.out.println("Introduce un numero para hacer su tabla de multiplicar");
+        int mult = entrada.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(mult + " * " + i + " = " + mult*i);
+        }
+         */
+/*
+        System.out.println("Cuantos numeros quieres sumar?");
+        int numeros = entrada.nextInt();
+        int acumula = 0;
+        for (int i = 1; i <= numeros; i++) {
+            System.out.println("Inserta nº" + i);
+            acumula += entrada.nextInt();
+        }
+        System.out.println("El resultado es " + acumula);
+ */
+        /*
+        System.out.println("Introduce el numero del que quieres hacer factorial");
+        int factorial = entrada.nextInt();
+        int resul = 1;
+        for (int i = factorial; i >= 1; i--) {
+            resul *= i;
+        }
+        System.out.println("El factorial de " +  factorial + " es " + resul);
+
+         */
+        /*
+        System.out.println("Introduce una cadena");
+        String cadena = entrada.nextLine();
+        for (int i = cadena.length(); i > 0; i--) {
+            System.out.println(cadena.charAt(i-1));
+        }
+        
+         */
+    }
+}
